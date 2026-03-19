0f221a0e2fe400d1c322090497421e2d1188a52c
Vinay Singh Jadon <vinay.jadon@wootz.work>
2026-03-19 16:26:32 +0530

    generateQIR - added new input variable 'sample checked'

--- generateQIR.js
+++ generateQIR.js
@@ -193,6 +193,7 @@ async function generateQIR(data) {
     ['RM Grade',    v(data.rm_grade)    ],
     ['Heat No.',    v(data.heat_no)     ],
     ['Qty',         v(data.order_qty)   ],
+    ['Samples Checked', v(data.samples_checked)],
     ['Verified By', v(data.verified_by)],
   ].filter(([, val]) => val !== null);  // drop pairs where value is empty/null
 
